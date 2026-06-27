f169ca07345ba83e8c8e57933d358d5e4eb52dfe
DevJAugusto-77 <dev.jaugusto@gmail.com>
2026-06-27 12:05:57 -0300

    feat: atualização 5

--- gestao-estoque/backend/server.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config'
-import app from './src/app'
-
-const port = Number(process.env.PORT ?? 4000)
-
-app.listen(port, () => {
-  console.log(`Backend rodando em http://localhost:${port}`)
-})
--- gestao-estoque/backend/src/db.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from './generated/prisma/index.js'
-import { PrismaNeon } from '@prisma/adapter-neon'
-
-const adapter = new PrismaNeon({
-  connectionString: process.env.DIRECT_URL ?? process.env.DATABASE_URL ?? '',
-})
-
-export const prisma = new PrismaClient({ adapter })
